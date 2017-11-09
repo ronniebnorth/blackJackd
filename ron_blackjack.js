8e3fe210fda8e4c4405afd0b82595123a2619b20
Ronnie North <ronnienorth@gmail.com>
2017-11-09 10:48:54 -0500

    rounds change

--- ron_blackjack.js
+++ ron_blackjack.js
@@ -3,8 +3,8 @@ const deckModel = [1,2,3,4,5,6,7,8,9,10,10,10,10,
 			   1,2,3,4,5,6,7,8,9,10,10,10,10,
 			   1,2,3,4,5,6,7,8,9,10,10,10,10,
 			   1,2,3,4,5,6,7,8,9,10,10,10,10];
-const numPlayers = 1;
-const rounds = 1000000;
+const numPlayers = 3;
+const rounds = 10;
 
 
 var played = 0;
